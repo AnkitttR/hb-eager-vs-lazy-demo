d7118f5d403376cdd9a9fc6ad2672b8e1b554960
Ankit <ankitthakur1998@gmail.com>
2022-04-20 14:10:59 +0530

    Mapping is done for id, first_name, last_name, email

--- src/com/luv2code/hibernate/demo/entity/Student.java
+++ src/com/luv2code/hibernate/demo/entity/Student.java
@@ -1,5 +1,40 @@
 package com.luv2code.hibernate.demo.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student") //Map Student class to student table //Note: Class name & table name could be different
 public class Student {
 
+	@Id
+	@Column(name="id") //Map variable id to column id of table
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="email")
+	private String email;
+	
+	public Student() {  //No Argument Constructor
+		
+	}
+		
 }
+
+
+
+
+
+
+
+
+
+
+
