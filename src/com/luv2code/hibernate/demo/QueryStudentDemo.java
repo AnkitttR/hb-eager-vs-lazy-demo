17962ff0810956b3639200907e5d047d819c5fb6
Ankit <ankitthakur1998@gmail.com>
2022-04-20 20:24:48 +0530

    QueryStudentDemo class modified

--- src/com/luv2code/hibernate/demo/QueryStudentDemo.java
+++ src/com/luv2code/hibernate/demo/QueryStudentDemo.java
@@ -22,18 +22,10 @@ public class QueryStudentDemo {
 		Session session = factory.getCurrentSession();
 		
 		try {
-			
-			//create a student object
-			System.out.println("Creating new student object...");
-			Student tempStudent = new Student("Paul","Wall","paul@luv2code.com");
-						
+												
 			//start a transaction
 			session.beginTransaction();
-			
-			//save the student object
-			System.out.println("Saving the student...");
-			session.save(tempStudent);
-			
+					
 			//commit transaction
 			session.getTransaction().commit();
 			System.out.println("Done!");
