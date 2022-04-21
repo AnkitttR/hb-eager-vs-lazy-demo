8f6e9a905470dc64e93e7def24290875937ca1bb
Ankit <ankitthakur1998@gmail.com>
2022-04-21 11:20:01 +0530

    UpdateStudentDemo class modified

--- src/com/luv2code/hibernate/demo/UpdateStudentDemo.java
+++ src/com/luv2code/hibernate/demo/UpdateStudentDemo.java
@@ -44,6 +44,11 @@ public class UpdateStudentDemo {
 			session = factory.getCurrentSession();
 			session.beginTransaction();
 			
+			//update email for all students
+			System.out.println("Update email for all students");
+			
+			session.createQuery("update Student set email='foo@gmail.com'")
+			
 			//commit the transaction
 			session.getTransaction().commit();
 		
