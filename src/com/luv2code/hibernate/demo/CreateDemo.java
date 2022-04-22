a27670f3a98e2b99a802d32fe6fb77b122a4d72d
Ankit <ankitthakur1998@gmail.com>
2022-04-22 13:09:32 +0530

    CreateDemo.java is modified

--- src/com/luv2code/hibernate/demo/CreateDemo.java
+++ src/com/luv2code/hibernate/demo/CreateDemo.java
@@ -25,18 +25,10 @@ public class CreateDemo {
 		Session session = factory.getCurrentSession();
 		
 		try {
-			
-			//create a student object
-			System.out.println("Creating new student object...");
-			Student tempStudent = new Student("Paul","Wall","paul@luv2code.com");
-						
+											
 			//start a transaction
 			session.beginTransaction();
-			
-			//save the student object
-			System.out.println("Saving the student...");
-			session.save(tempStudent);
-			
+						
 			//commit transaction
 			session.getTransaction().commit();
 			System.out.println("Done!");
