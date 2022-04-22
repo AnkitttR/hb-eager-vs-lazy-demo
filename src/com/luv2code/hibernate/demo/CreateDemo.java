b8c8054321a9b84231c05f928682be6266925ab4
Ankit <ankitthakur1998@gmail.com>
2022-04-22 13:07:27 +0530

    CreateDemo.java is modified

--- src/com/luv2code/hibernate/demo/CreateDemo.java
+++ src/com/luv2code/hibernate/demo/CreateDemo.java
@@ -5,6 +5,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
 import com.luv2code.hibernate.demo.entity.Student;
 
 
@@ -17,6 +18,7 @@ public class CreateDemo {
 		SessionFactory factory = new Configuration()
 				                        .configure("hibernate.cfg.xml")
 				                        .addAnnotatedClass(Instructor.class)
+				                        .addAnnotatedClass(InstructorDetail.class)
 				                        .buildSessionFactory();
 		
 		//create session
