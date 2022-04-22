2f8bf42efea8fdcf191fdcb541407de58056cc3b
Ankit <ankitthakur1998@gmail.com>
2022-04-22 12:47:22 +0530

    Constructor added to Instructor

--- src/com/luv2code/hibernate/demo/entity/Instructor.java
+++ src/com/luv2code/hibernate/demo/entity/Instructor.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -47,5 +48,17 @@ public class Instructor {
 		@OneToOne(cascade=CascadeType.ALL)
 		@JoinColumn(name="instructor_detail_id")
 		private InstructorDetail instructorDetail;
+		
+		public Instructor() { //No Argument constructor
+			
+		}
+
+		public Instructor(String firstName, String lastName, String email) {
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.email = email;
+		}
+		
+		
 	
 }
