0ae4f07e4f372c5c0e174bfa60f9c6db50526876
Ankit <ankitthakur1998@gmail.com>
2022-04-23 21:48:46 +0530

    Course & Instructor are joined through instructor_id column of database

--- src/com/luv2code/hibernate/demo/entity/Course.java
+++ src/com/luv2code/hibernate/demo/entity/Course.java
@@ -1,8 +1,12 @@
 package com.luv2code.hibernate.demo.entity;
 
-import java.security.PublicKey;
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -19,11 +23,17 @@ public class Course {
 	
 	//annotate fields 
 	
+	@Id  // Used for assigning primary key
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
 	private int id;
 	
+	@Column(name="title")
 	private String title;
 	
-	private Instructor instructor;
+	@ManyToOne    //Many courses to one instructor
+	@JoinColumn(name="instructor_id")  //Course & Instructor are connected by instructor_id column of database.
+	private Instructor instructor;  //Reference to Instructor class
 	
 	public Course() {  //No argument constructor
 			
@@ -32,4 +42,36 @@ public class Course {
 	public Course(String title) {
 		this.title = title;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", title=" + title + ", instructor=" + instructor + "]";
+	}
+	
+	
+	
 }
