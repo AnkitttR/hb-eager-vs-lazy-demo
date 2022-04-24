ae1b8bf3b597a9f55807130e2091265056cfc221
Ankit <ankitthakur1998@gmail.com>
2022-04-24 10:40:03 +0530

    CreateInstructorDemo class is modified

--- src/com/luv2code/hibernate/demo/CreateInstructorDemo.java
+++ src/com/luv2code/hibernate/demo/CreateInstructorDemo.java
@@ -4,6 +4,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.luv2code.hibernate.demo.entity.Course;
 import com.luv2code.hibernate.demo.entity.Instructor;
 import com.luv2code.hibernate.demo.entity.InstructorDetail;
 import com.luv2code.hibernate.demo.entity.Student;
@@ -19,6 +20,7 @@ public class CreateInstructorDemo {
 				                        .configure("hibernate.cfg.xml")
 				                        .addAnnotatedClass(Instructor.class)
 				                        .addAnnotatedClass(InstructorDetail.class)
+				                        .addAnnotatedClass(Course.class)
 				                        .buildSessionFactory();
 		
 		//create session
