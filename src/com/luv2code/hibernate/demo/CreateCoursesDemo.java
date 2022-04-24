91b13981fc01c258536fcf16d7dbf78db7db690f
Ankit <ankitthakur1998@gmail.com>
2022-04-24 12:29:57 +0530

    Course is saved to session

--- src/com/luv2code/hibernate/demo/CreateCoursesDemo.java
+++ src/com/luv2code/hibernate/demo/CreateCoursesDemo.java
@@ -39,12 +39,24 @@ public class CreateCoursesDemo {
 			int theId = 1;
 			Instructor tempInstructor = session.get(Instructor.class, theId);
 			
-			//create some courses
-			Course tempCourse1 = new Course("Air Guitar")
+			//Debugging Code
+			System.out.println("Value of tempInstructor" + tempInstructor);
 			
+			//create some courses
+			Course tempCourse1 = new Course("Air Guitar - The Ultimate Guide");
+			Course tempCourse2 = new Course("The Pinball Masterclass");
+						
 			//add courses to instructor
+			tempInstructor.add(tempCourse1);
+			tempInstructor.add(tempCourse2);
+			
+			//Debugging Code
+			System.out.println("Value of tempInstructor" + tempInstructor);
+			System.out.println("Value of tempInstructor" + tempInstructor);
 			
 			//save the courses
+			session.save(tempCourse1);
+			session.save(tempCourse2);
 			
 			//commit transaction
 			session.getTransaction().commit();
